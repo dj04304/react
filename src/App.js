0854ad40c380e2d1fd50991f0c5caf8c170ce389
dochi <dj043045@gmail.com>
2023-04-05 18:38:03 +0900

    react state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
 
 function Nav(props) {
 
@@ -19,7 +19,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
           <a id={t.id} href={"/read/"+t.id} onClick={(e) => {
               e.preventDefault();
-              props.onClickMode(e.target.id);
+              props.onClickMode(Number(e.target.id));
           }}>{t.title}</a>
         </li>)
   }
@@ -52,32 +52,52 @@ function Header(props) {
     </header>
 }
 
-function SignInBtn() {
-  return <button>로그인 버튼</button>
-}
-
-
 
 function App() {
+
+  // const _mode = useState("WELCOME");
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+
+
   const topics = [
     {id:1, title:"html", body:"html is ..."},
     {id:2, title:"css", body:"css is ..."},
     {id:3, title:"javascript", body:"javascript is ..."}
 
   ]
+  let content = null;
+  
+  if(mode == "WELCOME") {
+    content = <Article title="Welcome" body="Hello, Web"></Article>
 
+  } else if(mode == "READ") {
+    let title, body = null;
+    
+    for(let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+
+  }
 
   return (
     <div className="App">
         <Header title="WEB" onChangeMode={() => {
-          alert("Header");
+          setMode("WELCOME");
         }}></Header>
-        <Nav topics={topics} onClickMode={(id) => {
-          alert(id);
+        <Nav topics={topics} onClickMode={(_id) => {
+           setMode("READ");
+           setId(_id);
         }}></Nav>
-        <Article title="Welcome" body="Hello, Web"></Article>
-        <Article title="HI" body="Hello, Test"></Article>
-        <SignInBt></SignInBt>
+        {content}
     </div>
   );
 }
